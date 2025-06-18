f4a1fd274f7abb8955293161ddec446e91d6eec4
NelonArrive <nelon.arrive@gmail.com>
2025-06-18 10:27:57 +0500

    🎈refactor code in college

--- scripts/script.js
+++ scripts/script.js
@@ -154,12 +154,15 @@ forms.forEach(form => {
 // ================= BURGER MENU =================
 const burger = document.getElementById('burger')
 const menu = document.getElementById('menu')
+const menuNav = document.querySelector('menu__nav')
 const close = document.getElementById('close')
 
 burger.addEventListener('click', () => {
 	menu.classList.toggle('active')
+	menuNav.style.transform = 'translate(0)'
 })
 
 close.addEventListener('click', () => {
 	menu.classList.remove('active')
+	menuNav.style.transform = 'translate(100%)'
 })
